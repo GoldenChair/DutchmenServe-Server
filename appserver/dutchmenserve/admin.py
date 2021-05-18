c46e0ba27180df7d336e881c57494fa9cf1b7c94
Allison Liu <ajl008@lvc.edu>
2021-05-18 16:39:41 -0400

    notes from AL

--- appserver/dutchmenserve/admin.py
+++ appserver/dutchmenserve/admin.py
@@ -14,18 +14,3 @@ admin.site.register(EventInterest)
 admin.site.register(Membership)
 admin.site.register(Leadership)
 admin.site.register(Registration)
-
-
-# cd appserver
-# python manage.py runserver 5455  #localhost or emulator
-# python manage.py runserver 10.2.194.102:5455  #computer
-
-# python manage.py migrate
-# python manage.py makemigrations dutchmenserve  -- tell Django you made changes to models
-# python manage.py sqlmigrate dutchmenserve #### -- prints to screen, check what Django will do
-# python manage.py migrate -- rerun migrate to create tables/apply changes
-
-# python manage.py createsuperuser -- create admin
-# Username: admin
-# Email address: admin@lvc.edu
-# Password: dutchmenserve
